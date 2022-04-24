64cbf83d2f667fe5cccdeb599347df115933c0c1
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-04-24 11:26:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppRegExTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppRegExTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("input.txt");
+
+            Console.WriteLine(text);
+
+            Regex regex = new Regex(@"[0-9]");
+            MatchCollection matches = regex.Matches(text);
+
+            for(int i = 0; i < matches.Count; i++)
+            {
+                Console.WriteLine(matches[i].ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
